4057ca6dd8111ca9ae969f4a13ddfefc91adbffa
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-12-15 19:20:48 +0000

    ez

--- Scoreboard.java
+++ Scoreboard.java
@@ -8,3 +8,51 @@
 */
 
 // Write the class below
+
+public class Scoreboard
+{
+    private String team1;
+    private int team1Score;
+    private String team2;
+    private int team2Score;
+    private int turnCount;
+
+    public Scoreboard(String t1, String t2)
+    {
+        team1 = t1;
+        team2 = t2;
+        turnCount = 1;
+        team1Score = 0;
+        team2Score = 0;
+    }
+
+    public void recordPlay(int points)
+    {
+        if (points == 0)
+        {
+            turnCount++;
+        }
+        else if (turnCount % 2 == 1)
+        {
+            team1Score += points;
+        }
+        else if (turnCount % 2 == 0)
+        {
+            team2Score += points;
+        }
+    }
+
+    public String getScore()
+    {
+        String activeTeam;
+        if (turnCount % 2 == 1)
+        {
+            activeTeam = team1;
+        }
+        else
+        {
+            activeTeam = team2;
+        }
+        return team1Score + "-" + team2Score + "-" + activeTeam;
+    }
+}
